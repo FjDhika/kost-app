3bbddb45ed69a60140531ddabf51600e6884d5a5
FjDhika <fajarhandika12@gmail.com>
2022-04-23 19:58:17 +0700

    fix: user seeder add credit

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,24 +15,26 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name'=>'owner',
-            'email'=>'owner@gmail.com',
-            'role_id'=>1,
-            'password'=>bcrypt('owner@password'),
+            'name' => 'owner',
+            'email' => 'owner@gmail.com',
+            'role_id' => 1,
+            'password' => bcrypt('owner@password'),
         ]);
 
         DB::table('users')->insert([
-            'name'=>'regular_user',
-            'email'=>'regular_user@gmail.com',
-            'role_id'=>2,
-            'password'=>bcrypt('regular_user@password'),
+            'name' => 'regular_user',
+            'email' => 'regular_user@gmail.com',
+            'role_id' => 2,
+            'token' => 20,
+            'password' => bcrypt('regular_user@password'),
         ]);
 
         DB::table('users')->insert([
-            'name'=>'premium_user',
-            'email'=>'premium_user@gmail.com',
-            'role_id'=>3,
-            'password'=>bcrypt('premium_user@password'),
+            'name' => 'premium_user',
+            'email' => 'premium_user@gmail.com',
+            'role_id' => 3,
+            'token' => 40,
+            'password' => bcrypt('premium_user@password'),
         ]);
     }
 }
